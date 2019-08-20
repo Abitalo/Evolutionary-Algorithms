2c043a38e7e8366e5df8bfe5589d806d3e4279f5
Lancelot <alex.von.bryant@gmail.com>
2019-08-20 11:17:26 +0800

    supports higher feature dim, excluded gif code
    Mutation changed for np.random.choice can only sample (-1,1) shape individuals, so we use this only for sampling index.
    In np.random.choice must set replace =False, which decrease the performance by 3x, but it's necessary.
    Gif generation code I tried on CentOS and MacOS, some build issue about Imagemagick and matplotlib writer remains to be fixed. So I excluded it from this file.

--- DE.py
+++ DE.py
@@ -1,78 +1,68 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
-from matplotlib.animation import FuncAnimation
+# from matplotlib.animation import FuncAnimation
 
+# Updated in 20 Oct. 2019, supports higher dimensions of feature space.
 
-# This file implemented Differential Evolution Algorithm(DE) to search the maximum value for function obj_fn(x), 
+# This file implemented Differential Evolution Algorithm(DE) to search the global sub-optimal value for function obj_fn(x), 
 # which you can replace it with other function if you like.
 # For practice purpose, might be wrong and could be updated in any time.
 
 
 # the objectve function made up by personal choice.
-def obj_fn(x):
-    if not np.all(x):
-        return None
-    
+def obj_fn(x):   
     if not isinstance(x, np.ndarray):
         x = np.asarray(x)
         
-    ret = np.sin(x-3)*np.sin(0.5*x-3)-np.log(x**2+1)
-    return ret
+    ret = ((x[:,0]-12)**2) + ((x[:,1]-8)**2) # triviously, global minimum is (12,8)
+#     ret = np.sin(x-3)*np.sin(0.5*x-3)-np.log(x**2+1) # from the plot, global maximum near 0.2
 
+    # negative symbol is needed in minimum search scenario.
+    return -ret.squeeze() 
 
 
 def mutate(P, f=0.9):
-    # empty placeholder for mutated population, with same size to original(or previous) population.
     M = np.empty_like(P)
+    
 
-    # iterate individual c in previous population, mutate by difference from  2 randomly individuals picked from the rest population.
-    for i, c in enumerate(P):
-        
-        # randomly pick 2 individuals from the subset in which the current individual was excluded.
-        a,b = np.random.choice(np.concatenate([P[:i],P[i+1:]]),2)
+    # randomly pick 2 index of individuals from subsets other than current idx i, for i in range(N)
+    indice = [np.random.choice(np.concatenate([list(range(i)),list(range(i+1,N))]), 2, replace=False) for i in range(N)]
+    indice = np.asarray(indice, dtype=np.int)
 
-        # mutation.
-        M[i] = c + F*(a-b)
+    # c = f*(a-b)
+    M = f * (P[indice[:,0]] - P[indice[:,1]])
 
     return M
 
 
 def cross(P, M, cr=0.5):
     # crossover subject to cross_rate cr.
-    new_P = np.where(np.random.rand(N)<cr,M,P)
+    new_P = np.where(np.random.rand(N,D)<cr,M,P)
     
     return new_P
 
 
 def select(P):
-    # remove the illegal individuals.
-    P = P[abs(P)<50]
-    fitness = obj_fn(P)
-    # keept those individuals with top K fitness.
+    # 修正边界
+    P = P[np.all(abs(P)<50,axis=-1)]
+    fitness = obj_fn(P)   
+
+    #top K largest fitness
     new_P = P[np.argpartition(-fitness,N)][:N]
 
     return new_P   
 
 if __name__ == '__main__':
-    # plot the objective function.
-    X = list(np.linspace(-50,50,1001))
-    Y = []
-    for x in np.linspace(-50,50,1001):
-        Y.append(obj_fn(x))
-    plt.plot(X,Y)
-
-    # basic hyper parameter
-    N = 100 # population size
-    F = 0.9 # scale factor
-    CR = 0.5 # cross rate
-    max_G = 20 # max generation
-
-    # initial population randomly sampled from (-50,50).
-    P = np.random.rand(N)*100-50
+    N = 100     # population size
+    D = 2       # dimensions of feature space 
+    F = 0.9     # mutation rate
+    CR = 0.5    # cross rate
+    max_G = 100 # max generation
+    P = np.random.rand(N,D)*100-50 # uniform sampled from (-50,50)
     
     # for gif generating purpose.
-    P_seq = [[]] 
+    P_seq = []
     
     try:
         with tqdm(range(max_G)) as t:
@@ -82,28 +72,17 @@ if __name__ == '__main__':
                 # record current population state.
                 P_seq.append(P)
                 
-                M = mutate(P)
-                new_P = cross(P,M)  
-                P = select(np.concatenate([P,new_P], axis=0))
+                M = mutate(P,F)
+                new_P = cross(P,M,CR)
+                P = select(np.concatenate([P,new_P]))
 
     except KeyboardInterrupt:
         t.close()
         raise
     finally:
-        t.close
+        t.close()
         
     # record the final population state.
     P_seq.append(P)
     
-    
-    # generate animation gif.
-    fig, ax = plt.subplots()
-    fig.set_tight_layout(True)
-    line = ax.plot(X,Y)
-    sc = ax.scatter(None,None,c='red')
-    def update(t):
-        ax.set_xlabel(f'iteration {t}')
-        sc.set_offsets(np.c_[P_seq[t],obj_fn(P_seq[t])])
-    
-    anim = FuncAnimation(fig, update, frames=np.arange(0, 21), interval=200)
-    anim.save('de_convergence.gif', dpi=80, writer='imagemagick')
+    print(f'the sub-optimal found in {max_G} generations is: {P.mean(axis=0)}')
